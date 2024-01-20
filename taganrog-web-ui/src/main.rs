fed65def720931710dbda61cd2ba4bd64bf01fdd
darkcodi <trooper982@gmail.com>
2024-01-20 17:32:33 -0600

    Add axum and askama packages to web-ui project

--- taganrog-web-ui/src/main.rs
+++ taganrog-web-ui/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::info;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
+    info!("hello, web server!");
 }
