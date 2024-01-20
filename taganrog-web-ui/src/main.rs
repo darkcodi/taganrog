8a50a2933a6524295111d013a7403de1d172c1f8
darkcodi <trooper982@gmail.com>
2024-01-20 17:53:10 -0600

    Add askama hello world

--- taganrog-web-ui/src/main.rs
+++ taganrog-web-ui/src/main.rs
@@ -1,3 +1,10 @@
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use tracing::info;
 use tracing_subscriber::util::SubscriberInitExt;
 
@@ -7,5 +14,43 @@ async fn main() {
         .with_max_level(tracing::Level::INFO)
         .init();
 
-    info!("hello, web server!");
+    info!("initializing router...");
+    let router = Router::new().route("/", get(hello));
+
+    let addr = "[::]:1775";
+    let listener = tokio::net::TcpListener::bind(addr).await.expect("failed to bind to address");
+    info!("listening on {}", &addr);
+    axum::serve(listener, router).await.expect("error running HTTP server");
+}
+
+async fn hello() -> impl IntoResponse {
+    let template = HelloTemplate {};
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate;
+
+/// A wrapper type that we'll use to encapsulate HTML parsed by askama into valid HTML for axum to serve.
+struct HtmlTemplate<T>(T);
+
+/// Allows us to convert Askama HTML templates into valid HTML for axum to serve in the response.
+impl<T> IntoResponse for HtmlTemplate<T>
+    where
+        T: Template,
+{
+    fn into_response(self) -> Response {
+        // Attempt to render the template with askama
+        match self.0.render() {
+            // If we're able to successfully parse and aggregate the template, serve it
+            Ok(html) => Html(html).into_response(),
+            // If we're not, return an error or some bit of fallback HTML
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
 }
