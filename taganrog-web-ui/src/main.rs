ba67bfcbeb4eb4a22c30b0e035b8900b021485db
darkcodi <trooper982@gmail.com>
2024-01-20 22:33:33 -0600

    Add search page template

--- taganrog-web-ui/src/main.rs
+++ taganrog-web-ui/src/main.rs
@@ -1,4 +1,5 @@
 use axum::{routing::get, Router};
+use axum::extract::Query;
 use axum::response::IntoResponse;
 use axum_macros::FromRef;
 use axum_template::engine::Engine;
@@ -6,9 +7,10 @@ use axum_template::{Key, RenderHtml};
 use minijinja::Environment;
 use tracing::info;
 use tracing_subscriber::util::SubscriberInitExt;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 const INDEX_TEMPLATE: &str = include_str!("../templates/index.html");
+const SEARCH_TEMPLATE: &str = include_str!("../templates/search.html");
 
 #[tokio::main]
 async fn main() {
@@ -16,14 +18,15 @@ async fn main() {
         .with_max_level(tracing::Level::INFO)
         .init();
 
-    info!("initializing router...");
+    info!("initializing templates...");
     let mut jinja = Environment::new();
-    jinja
-        .add_template("/", INDEX_TEMPLATE)
-        .unwrap();
+    jinja.add_template("/", INDEX_TEMPLATE).unwrap();
+    jinja.add_template("/search", SEARCH_TEMPLATE).unwrap();
 
+    info!("initializing router...");
     let router = Router::new()
         .route("/", get(index))
+        .route("/search", get(search))
         .with_state(AppState {
             engine: Engine::from(jinja),
         });;
@@ -44,10 +47,31 @@ struct AppState {
 #[derive(Debug, Serialize)]
 pub struct IndexPageContext;
 
+#[derive(Debug, Serialize)]
+pub struct SearchPageContext {
+    query: String,
+}
+
 async fn index(
     engine: AppEngine,
     Key(key): Key,
 ) -> impl IntoResponse {
     let ctx = IndexPageContext;
+    RenderHtml(key, engine, ctx)
+}
+
+#[derive(Deserialize)]
+struct SearchQuery {
+    q: String,
+}
+
+async fn search(
+    engine: AppEngine,
+    Key(key): Key,
+    Query(query): Query<SearchQuery>,
+) -> impl IntoResponse {
+    let ctx = SearchPageContext {
+        query: query.q,
+    };
     RenderHtml(key, engine, ctx)
 }
