ba57f5a72e397e24edfae1205d3ca25cc99d5ca5
darkcodi <trooper982@gmail.com>
2024-02-03 15:49:23 -0600

    Add tag include/exclude on search page

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -6,14 +6,27 @@ pub mod hash_utils;
 pub mod str_utils;
 
 pub fn normalize_query(query: &str) -> String {
-    let mut tags = query.split(" ")
+    let initial_tags = query.split(" ")
         .map(|x| x.trim()) // remove leading and trailing whitespaces
         .filter(|x| !x.is_empty()) // remove empty strings (e.g. multiple spaces)
-        .map(|x| x.slugify().to_string()) // convert to slug
+        .collect::<Vec<&str>>();
+    let tags_to_exclude = initial_tags.iter()
+        .filter(|x| x.starts_with("-")) // find tags to exclude
+        .map(|x| x.slugify()) // and slugify them
+        .filter(|x| !x.is_empty()) // remove empty strings
+        .collect::<Vec<String>>();
+    let final_tags = initial_tags.iter()
+        .map(|x| x.slugify().to_string()) // slugify all tags
+        .filter(|x| !x.is_empty()) // remove empty strings
+        .filter(|x| !tags_to_exclude.contains(&x)) // remove tags to exclude
         .unique() // filter out duplicates
         .collect::<Vec<String>>();
-    if tags.len() > 0 && query.ends_with(" ") {
-        tags.push("".to_string());
+    let mut normalized_query = final_tags.join(" ");
+
+    // append ' ' if query ends with a space
+    if normalized_query.len() > 0 && query.ends_with(" ") {
+        normalized_query.push(' ');
     }
-    tags.join(" ")
+
+    normalized_query
 }
