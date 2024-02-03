2c421b39985b321c6e2a707b3087559c690cd141
darkcodi <trooper982@gmail.com>
2024-02-03 15:26:14 -0600

    Change behaviour for tag clicking on media page

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use crate::utils::str_utils::StringExtensions;
 
 pub mod vec_utils;
@@ -5,7 +6,12 @@ pub mod hash_utils;
 pub mod str_utils;
 
 pub fn normalize_query(query: &str) -> String {
-    let mut tags = query.split(" ").map(|x| x.trim()).filter(|x| !x.is_empty()).map(|x| x.slugify().to_string()).collect::<Vec<String>>();
+    let mut tags = query.split(" ")
+        .map(|x| x.trim()) // remove leading and trailing whitespaces
+        .filter(|x| !x.is_empty()) // remove empty strings (e.g. multiple spaces)
+        .map(|x| x.slugify().to_string()) // convert to slug
+        .unique() // filter out duplicates
+        .collect::<Vec<String>>();
     if tags.len() > 0 && query.ends_with(" ") {
         tags.push("".to_string());
     }
