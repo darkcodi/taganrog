25ee155c81d8c4916338b3fc68bd96557979f42b
darkcodi <trooper982@gmail.com>
2024-10-25 14:22:52 -0500

    Fix tag/untag CLI commands

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,5 +1,4 @@
 use std::path::PathBuf;
-use log::{error, info};
 use crate::client::TaganrogClient;
 use crate::entities::{MediaPage, TagsAutocomplete};
 use crate::error::TaganrogError;
@@ -7,8 +6,8 @@ use crate::storage::Storage;
 use crate::utils::normalize_query;
 
 pub async fn tag_media<T: Storage>(client: &mut TaganrogClient<T>, filepath: &str, tag: &String) -> Result<bool, TaganrogError> {
-    let canonical_filepath = canonicalize(filepath);
-    let mut media = client.create_media_from_file(&canonical_filepath).await?;
+    let filepath: PathBuf = filepath.into();
+    let mut media = client.create_media_from_file(&filepath).await?;
     if client.get_media_by_id(&media.id).is_none() {
         media = client.add_media(media).await?.safe_unwrap();
     }
@@ -17,8 +16,8 @@ pub async fn tag_media<T: Storage>(client: &mut TaganrogClient<T>, filepath: &st
 }
 
 pub async fn untag_media<T: Storage>(client: &mut TaganrogClient<T>, filepath: &str, tag: &String) -> Result<bool, TaganrogError> {
-    let canonical_filepath = canonicalize(filepath);
-    let media = client.create_media_from_file(&canonical_filepath).await?;
+    let filepath: PathBuf = filepath.into();
+    let media = client.create_media_from_file(&filepath).await?;
     let was_removed = client.remove_tag_from_media(&media.id, tag).await?;
     Ok(was_removed)
 }
@@ -38,22 +37,4 @@ pub async fn search_media<T: Storage>(client: &TaganrogClient<T>, tags: Vec<Stri
         return client.get_all_media(page_size, page_index);
     }
     client.search_media(&normalized_query, page_size, page_index)
-}
-
-fn canonicalize(filepath: &str) -> PathBuf {
-    let canonical_filepath_result = std::fs::canonicalize(filepath);
-    if canonical_filepath_result.is_err() {
-        let err = canonical_filepath_result.err().unwrap();
-        if err.kind() == std::io::ErrorKind::NotFound {
-            error!("file not found: {}", filepath);
-            std::process::exit(1);
-        } else {
-            error!("io error: {}", err);
-            std::process::exit(1);
-        }
-    }
-
-    let canonical_filepath = canonical_filepath_result.unwrap();
-    info!("file: {:?}", canonical_filepath);
-    canonical_filepath
 }
